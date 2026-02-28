a4f482d61909d01c38ca06fdfdf040fcb9f7a3a8
anukulKun <anukulpandit30s@gmail.com>
2026-02-28 11:00:00 +0530

    feat: add launchpad config

---
+++ src/configs/launchpad.ts
@@ -0,0 +1,66 @@
+import type { LaunchpadData } from "~/types";
+
+const launchpadApps: LaunchpadData[] = [
+  {
+    id: "about-me",
+    title: "About Me",
+    img: "logo/121.jpg",
+    appId: "about-me"
+  },
+  {
+    id: "resume",
+    title: "Resume",
+    img: "img/icons/launchpad/resume.png",
+    appId: "resume"
+  },
+  {
+    id: "projects",
+    title: "Projects",
+    img: "img/icons/projects.svg",
+    appId: "projects"
+  },
+  {
+    id: "irc-chat",
+    title: "IRC Chat",
+    img: "img/icons/irc-chat.svg",
+    appId: "irc-chat"
+  },
+  {
+    id: "safari",
+    title: "Safari",
+    img: "img/icons/safari.png",
+    appId: "safari"
+  },
+  {
+    id: "vscode",
+    title: "VSCode",
+    img: "img/icons/vscode.png",
+    appId: "vscode"
+  },
+  {
+    id: "facetime",
+    title: "FaceTime",
+    img: "img/icons/facetime.png",
+    appId: "facetime"
+  },
+  {
+    id: "terminal",
+    title: "Terminal",
+    img: "img/icons/terminal.png",
+    appId: "terminal"
+  },
+  {
+    id: "typora",
+    title: "Typora",
+    img: "img/icons/typora.png",
+    appId: "typora"
+  },
+  {
+    id: "github",
+    title: "Github",
+    img: "img/icons/github.png",
+    link: "https://github.com/anukulKun"
+  }
+];
+
+export default launchpadApps;
