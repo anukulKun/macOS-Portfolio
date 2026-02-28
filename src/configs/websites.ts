3dfe44c9a91c4dff63bd25634bf77b5b5a6ffd38
anukulKun <anukulpandit30s@gmail.com>
2026-02-28 14:00:00 +0530

    feat: add websites config for Safari

---
+++ src/configs/websites.ts
@@ -0,0 +1,109 @@
+import type { WebsitesData } from "~/types";
+
+const websites: WebsitesData = {
+  favorites: {
+    title: "SNS Links",
+    sites: [
+      {
+        id: "my-blog",
+        title: "Website",
+        img: "logo/121.jpg",
+        link: "https://anukul.xyz/",
+        inner: true
+      },
+      {
+        id: "my-github",
+        title: "Github",
+        img: "img/sites/github.svg",
+        link: "https://github.com/anukulKun"
+      },
+      {
+        id: "my-linkedin",
+        title: "Linkedin",
+        img: "img/sites/linkedin.svg",
+        link: "https://www.linkedin.com/in/anukulKun"
+      },
+      {
+        id: "my-twitter",
+        title: "Twitter",
+        img: "img/sites/twitter.svg",
+        link: "https://www.twitter.com/anukulKun"
+      },
+      {
+        id: "my-email",
+        title: "Email",
+        img: "img/sites/gmail.svg",
+        link: "mailto:anukulpandit30s@gmail.com"
+      }
+    ]
+  },
+  freq: {
+    title: "Frequently Visited",
+    sites: [
+      {
+        id: "github",
+        title: "Github",
+        img: "img/sites/github.svg",
+        link: "https://github.com/"
+      },
+      {
+        id: "twitter",
+        title: "Twitter",
+        img: "img/sites/twitter.svg",
+        link: "https://www.twitter.com/"
+      },
+      {
+        id: "dribbble",
+        title: "Dribbble",
+        img: "img/sites/dribbble.svg",
+        link: "https://dribbble.com/"
+      },
+      {
+        id: "pinterest",
+        title: "Pinterest",
+        img: "img/sites/pinterest.svg",
+        link: "https://www.pinterest.com/"
+      },
+      {
+        id: "bilibili",
+        title: "Bilibili",
+        img: "img/sites/bilibili.svg",
+        link: "https://www.bilibili.com/"
+      },
+
+      {
+        id: "leetcode",
+        title: "LeetCode",
+        img: "img/sites/leetcode.svg",
+        link: "https://leetcode.com/"
+      },
+      {
+        id: "reddit",
+        title: "Reddit",
+        img: "img/sites/reddit.svg",
+        link: "https://www.reddit.com/"
+      },
+      {
+        id: "hacker-news",
+        title: "Hacker News",
+        img: "img/sites/hacker.svg",
+        link: "https://news.ycombinator.com/"
+      },
+ 
+      {
+        id: "steam",
+        title: "Steam",
+        img: "img/sites/steam.svg",
+        link: "https://store.steampowered.com/"
+      },
+      {
+        id: "nazo",
+        title: "Nazo",
+        link: "https://nazo.one-story.cn"
+      },
+
+    ]
+  }
+};
+
+export default websites;
